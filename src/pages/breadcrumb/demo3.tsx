1d8027c99e6592593c4c22741e1bd31f29f90e99
mj <mj@kohoh.cn>
2021-06-22 21:01:13 +0800

    code breadcrumb

---
+++ src/pages/breadcrumb/demo3.tsx
@@ -0,0 +1,10 @@
+import { Breadcrumb } from "antd"
+
+export default ()=>{
+  return <Breadcrumb separator='>'>
+  <Breadcrumb.Item>Home</Breadcrumb.Item>
+  <Breadcrumb.Item href=''>Application Center</Breadcrumb.Item>
+  <Breadcrumb.Item href=''>Application List</Breadcrumb.Item>
+  <Breadcrumb.Item>An Application</Breadcrumb.Item>
+  </Breadcrumb>
+}
