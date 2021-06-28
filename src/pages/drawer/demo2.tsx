7545ab4ecbf00dfc704e5f7bb7994e88d8d8abd7
mj <mj@kohoh.cn>
2021-06-28 11:53:20 +0800

    code drawer demo2

---
+++ src/pages/drawer/demo2.tsx
@@ -0,0 +1,39 @@
+import { Button, Drawer, Radio, RadioChangeEvent, Space } from 'antd';
+import { useState } from 'react';
+
+export default () => {
+  const [visible, setVisible] = useState(false);
+  const [placement, setPlacement] = useState<
+    'right' | 'top' | 'left' | 'bottom'
+  >('right');
+
+  const showDrawer = () => setVisible(true);
+  const onClose = () => setVisible(false);
+  const onChange = (e: RadioChangeEvent) => setPlacement(e.target.value);
+  return (
+    <>
+      <Space>
+        <Radio.Group defaultValue={placement} onChange={onChange}>
+          <Radio value="top">top</Radio>
+          <Radio value="right">right</Radio>
+          <Radio value="bottom">bottom</Radio>
+          <Radio value="left">left</Radio>
+        </Radio.Group>
+      </Space>
+      <Button type="primary" onClick={showDrawer}>
+        Open
+      </Button>
+      <Drawer
+        title="Basic Drawer"
+        closable={false}
+        placement={placement}
+        onClose={onClose}
+        visible={visible}
+      >
+        <p>Some Content...</p>
+        <p>Some Content...</p>
+        <p>Some Content...</p>
+      </Drawer>
+    </>
+  );
+};
